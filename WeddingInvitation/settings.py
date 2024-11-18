5d8d25e51622f23b1cbb36e1ba71b0e0bdf9ad4b
Daniel Babativa <da.babativa@uniandes.edu.co>
2024-11-18 15:37:47 -0500

    Deployment configs

--- WeddingInvitation/settings.py
+++ WeddingInvitation/settings.py
@@ -88,7 +88,20 @@ DATABASES = {
     }
 }
 
+# from dotenv import load_dotenv
 
+# load_dotenv()
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": "5432",
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
