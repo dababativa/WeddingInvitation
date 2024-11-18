0bcd9e0654777aa7667c83fe109edd7f007b555d
Daniel Babativa <da.babativa@uniandes.edu.co>
2024-11-18 14:11:10 -0500

    Deployment configs

--- build_files.sh
+++ build_files.sh
@@ -9,3 +9,5 @@ echo "Migrating Database..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
+echo "Collecting Static Files..."
+python3 manage.py collectstatic --noinput
